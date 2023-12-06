e05e13ee33fb036150a9cdd4d04f5cb9ba23601b
Ludvig <ludvig.vinoy@hotmail.com>
2023-12-06 09:42:32 +0100

    PersonKlass

---
+++ AdressBook/Interfaces/IPerson.cs
@@ -0,0 +1,13 @@
+﻿namespace AdressBook.Interfaces
+{
+    public interface IPerson
+    {
+        string City { get; set; }
+        string Email { get; set; }
+        string FirstName { get; set; }
+        string LastName { get; set; }
+        int PhoneNumber { get; set; }
+        string StreetName { get; set; }
+        int StreetNumber { get; set; }
+    }
+}
---
+++ AdressBook/Models/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdressBook.Interfaces;
+
+namespace AdressBook.Models
+{
+    public class Person : IPerson
+    {
+        public string FirstName { get; set; } = null!;
+
+        public string LastName { get; set; } = null!;
+
+        public int PhoneNumber { get; set; } = 0!;
+
+        public string Email { get; set; } = null!;
+
+        public string StreetName { get; set; } = null!;
+
+        public int StreetNumber { get; set; } = 0;
+
+        public string City { get; set; } = null!;
+    }
+}
